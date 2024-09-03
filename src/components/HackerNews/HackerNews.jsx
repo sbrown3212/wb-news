840964556ac929363ee83c3740360770ed1f0c21
Stephen Brown <stephen.jr36@gmail.com>
2024-09-03 14:56:20 -0600

    updated HackerNews component to use useSelector to get articles and loading. Added useEffect to request articles

--- src/components/HackerNews/HackerNews.jsx
+++ src/components/HackerNews/HackerNews.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { requestArticles } from '../../reducers/hackerNewsReducer.js';
+import { useEffect } from 'react';
 import Card from '../shared/Card/Card.jsx';
 import Loading from '../shared/Loading/Loading.jsx';
 
 export default function HackerNews() {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const articles = useSelector((state) => state.articles);
+  const loading = useSelector((state) => state.loading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {dispatch(requestArticles)}, [])
+  
 
   const articleCards = articles.map((article) => <Card key={article.id} article={article} />);
   return (
