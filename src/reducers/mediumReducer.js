654fcdc48b20d2b56073f7b3533e65592db1e657
Stephen Brown <stephen.jr36@gmail.com>
2024-09-03 15:00:37 -0600

    created initialState object and mediumReducer function

---
+++ src/reducers/mediumReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  loading: false,
+  articles: [],
+}
+
+export default function mediumReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
