f9b61c86972f79a37baa2aebaf76ec40f77ac269
Stephen Brown <stephen.jr36@gmail.com>
2024-09-03 15:59:22 -0600

    changed useStates to useSelectors, added useDispatch, added useEffect to request articles

--- src/components/Reddit/Reddit.jsx
+++ src/components/Reddit/Reddit.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import Card from '../shared/Card/Card.jsx';
 import Loading from '../shared/Loading/Loading.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { requestArticles } from '../../reducers/redditReducer.js';
 
 export default function Reddit() {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const articles = useSelector((state) => state.reddit.articles);
+  const loading = useSelector((state) => state.reddit.loading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {dispatch(requestArticles)}, []);
+
   const articleCards = articles.map((article) => <Card key={article.id} article={article} />);
 
   return (
