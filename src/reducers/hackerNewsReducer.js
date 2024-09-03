e37b72b337fb5e8f676d75f41e05dc4b3a0d12f9
Stephen Brown <stephen.jr36@gmail.com>
2024-09-03 13:58:08 -0600

    created hackerNewsReducer, Redux store, and setup provider in main.jsx

---
+++ src/reducers/hackerNewsReducer.js
@@ -0,0 +1,13 @@
+
+
+const initialState = {
+  loading: false,
+  articles: [],
+}
+
+export default hackerNewsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
