74696c30fe4f3a0cf3d2fe76827184d7c4119ddd
Stephen Brown <stephen.jr36@gmail.com>
2024-09-03 14:27:11 -0600

    added requestArticles function. Added hackerNewsReducer case logic

--- src/reducers/hackerNewsReducer.js
+++ src/reducers/hackerNewsReducer.js
@@ -1,12 +1,42 @@
-
+import axios from "axios"
+import { useDispatch } from "react-redux"
 
 const initialState = {
   loading: false,
   articles: [],
 }
 
-export default hackerNewsReducer = (state = initialState, action) => {
+const REQUEST_ARTICLES = 'REQUEST_ARTICLES';
+const PENDING = 'PENDING';
+
+// QUESTION: why do I not need to import useDispatch?
+export const requestArticles = async (dispatch) => {
+  dispatch({
+    type: PENDING
+  });
+
+  let articles = await axios.get('/api/hacker-news').then((res) => res.data);
+
+  dispatch({
+    type: REQUEST_ARTICLES,
+    payload: articles,
+  })
+}
+
+export default function hackerNewsReducer(state = initialState, action) {
+
   switch (action.type) {
+    case 'PENDING':
+      return {
+        ...state,
+        loading: true,
+      }
+    case 'REQUEST_ARTICLES':
+      return { // QUESTION: why do I not need to spread 'state'?
+        loading: false,
+        articles: action.payload,
+      }
+
     default:
       return state
   }
