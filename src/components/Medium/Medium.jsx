2db2602155f51d0de65357885a5716e15fb3a3d2
Stephen Brown <stephen.jr36@gmail.com>
2024-09-03 15:40:07 -0600

    added mediumReducer logic and requestArticles fn, updated Medium component with useSelectors and useEffect to request articles

--- src/components/Medium/Medium.jsx
+++ src/components/Medium/Medium.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import Card from '../shared/Card/Card.jsx';
 import Loading from '../shared/Loading/Loading.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { requestArticles } from '../../reducers/mediumReducer.js';
 
 export default function Medium() {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const articles = useSelector((state) => state.medium.articles);
+  const loading = useSelector((state) => state.medium.loading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {dispatch(requestArticles)}, [])
+
   const articleCards = articles.map((article) => <Card key={article.id} article={article} />);
 
   return (
